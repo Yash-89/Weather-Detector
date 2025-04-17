c9e10dc02dd16e9e0db55560ac7c2688afaa8d02
Yash-89 <yashproraj@gmail.com>
2025-04-17 06:11:47 +0530

    Refactor weather data retrieval and update temperature unit to Celsius

--- weather/views.py
+++ weather/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 import json
-import urllib.request
+from urllib.request import urlopen
+from urllib.parse import quote
 
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=e4f4ba067cb4975f70295e42e3acb407').read()
+        city_copy = quote(city)
+        res = urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city_copy}&appid=e4f4ba067cb4975f70295e42e3acb407').read()
         json_data = json.loads(res)
         data = {
             "country_code": str(json_data['sys']['country']),
             "coordinates": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
-            "temp": str(json_data['main']['temp']) + ' K',
+            "temp": str(round(json_data['main']['temp'] - 273.15, 2)) + ' °C',
             "pressure": str(json_data['main']['pressure']),
             "humidity": str(json_data['main']['humidity']),
         }
